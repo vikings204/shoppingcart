b3714ed4545857402f423d3c937d36639aeb8ec5
xorbotz <116655699+xorbotz@users.noreply.github.com>
2023-04-04 00:38:04 +0000

    trying stuff

---
+++ src/main/java/frc/robot/SwerveModule2.java
@@ -0,0 +1,23 @@
+package frc.robot;
+//package frc.robot;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import frc.lib.Conversions;
+
+//import frc.lib.util.CTREModuleState;
+//import frc.lib.util.SwerveModuleConstants;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.sensors.CANCoder;
+public class SwerveModule2 {
+    
+}
